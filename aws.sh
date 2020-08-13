cc142b48b2e5511135e09cde3751d142bcb91312
K_shivasali <66362181+sbkotresh16@users.noreply.github.com>
2020-08-13 15:54:10 +0530

    Update aws.sh

--- aws.sh
+++ aws.sh
@@ -5,5 +5,5 @@ aws ec2 run-instances \
     --security-group-ids sg-0219d74c4c801bcc9 \
     --associate-public-ip-address \
     --key-name EC2 \
-    --region us-east-1
+    --region us-east-1 'touch kot'
     
